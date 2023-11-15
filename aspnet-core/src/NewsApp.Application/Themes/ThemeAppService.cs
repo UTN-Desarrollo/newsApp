2b41e204c64cea99dd2f9788a9e4ea1c88adc64b
Matías Noir <matiasnoir98@gmail.com>
2023-11-15 20:20:29 -0300

    cambios-Obtención de la información de listas de temas

--- aspnet-core/src/NewsApp.Application/Themes/ThemeAppService.cs
+++ aspnet-core/src/NewsApp.Application/Themes/ThemeAppService.cs
@@ -9,13 +9,33 @@ using Volo.Abp.Domain.Repositories;
 
 namespace NewsApp.Themes
 {
-    public class ThemeAppService : CrudAppService<Theme,ThemeDto,int>, 
+    public class ThemeAppService : NewsAppAppService, 
     IThemeAppService 
     {
-        public ThemeAppService(IRepository<Theme, int> repository)
-            : base(repository)
-        {
+        private readonly IRepository<Theme, int> _repository;
 
+        //Siempre que hagamos un appService, tenemos que heredar de NewsAppAppService
+        public ThemeAppService(IRepository<Theme, int> repository)
+        {
+            //IRepository es la interfaz con la cual podemos interactuar con la base de datos (para consultas o persistir informacion)
+            _repository = repository;
+        }
+
+        public async Task<ICollection<ThemeDto>> GetThemesAsync()
+        {
+            //para usar metodos asincronicos, se debe colocar el "await" antes
+            var themes = await _repository.GetListAsync();
+
+            return ObjectMapper.Map<ICollection<Theme>, ICollection<ThemeDto>>(themes);
+        }
+
+        //Prueba para obtener 1 solo tema
+        public async Task<ThemeDto> GetThemesAsync(int id)
+        {
+            //para usar metodos asincronicos, se debe colocar el "await" antes
+            var theme = await _repository.GetAsync(id);
+
+            return ObjectMapper.Map<Theme, ThemeDto>(theme);
         }
     }
 }
